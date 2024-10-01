2bda81d3d22d09c49dc914a7dc21c3668558df08
Ilter Ahmet <ilterahmet93@gmail.com>
2024-10-01 14:13:44 +0100

    correct index

--- breath-away/app/index.tsx
+++ breath-away/app/index.tsx
@@ -1,22 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
-import Welcome from "./welcome";
 
 const App = () => {
-  return <Welcome />;
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>App</Text>
+    </View>
+  );
 };
 
 export default App;
-
-// import { View, Text } from "react-native";
-// import React from "react";
-
-// const App = () => {
-//   return (
-//     <View className="flex-1 items-center justify-center bg-white">
-//       <Text>App</Text>
-//     </View>
-//   );
-// };
-
-// export default App;
