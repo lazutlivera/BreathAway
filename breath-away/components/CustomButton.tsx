78c184e3d9c280d44878764f810555c31b83b808
Mitchenya <Mitchellrjshepherd@outlook.com>
2024-10-01 10:43:49 +0100

    Add CustomButton logic

---
+++ breath-away/components/CustomButton.tsx
@@ -0,0 +1,36 @@
+import {Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+interface CustomButtonProps {
+    onPress:() => void,
+    textStyles?: string;
+    title: string,
+    containerStyles?: string,
+    isLoading: boolean
+
+}
+
+const CustomButton = ({
+  title,
+  onPress,
+  containerStyles = "",
+  textStyles = "",
+  isLoading,
+}: CustomButtonProps) => {
+  return (
+    <TouchableOpacity
+        onPress={onPress}
+      activeOpacity={0.7}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
